39e8114ad97889deaae362305ea30cd9b982089b
Pihu-Ananya <97041050+Pihu-Ananya@users.noreply.github.com>
2022-05-02 14:46:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-https://teachablemachine.withgoogle.com/models/72dqAUnBk/  
+//https://teachablemachine.withgoogle.com/models/72dqAUnBk/  
+function speak (){
+    var synth=window.speechSynthesis;
+speak_data1="The first prediction is "+Prediction1;
+speak_data2="The second pridiction is"+Prediction2;
+
+var Utter =new SpeechSynthesisUtterance(speak_data1+speak_data2);
+synth.speak(Utter);
+
+}
+ 
+speak()
