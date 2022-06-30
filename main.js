6e09e34c556ae0a2c3c491ac7aadbf7f39f0e3f0
Pihu-Ananya <97041050+Pihu-Ananya@users.noreply.github.com>
2022-06-30 15:49:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-//https://teachablemachine.withgoogle.com/models/72dqAUnBk/  
+
+
+
+Prediction1 = "";
+Prediction2 = "";
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("camera")
+Webcam.attach(camera);
+
+function takesnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured_img' src='" + data_uri + "'> "
+
+    });
+
+}
+console.log("ml5version", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/72dqAUnBk/model.json", model_loaded);
+
+function model_loaded() {
+    console.log("model_loaded");
+
+} 
 function speak (){
     var synth=window.speechSynthesis;
 speak_data1="The first prediction is "+Prediction1;
@@ -9,4 +37,25 @@ synth.speak(Utter);
 
 }
  
-speak()
+
+function check() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img, got_result);
+}
+
+function got_result(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        Prediction1 = results[0].label;
+        Prediction2 = results[1].label;
+        document.getElementById("result_1").innerHTML = Prediction1;
+        document.getElementById("result_2").innerHTML = Prediction2;
+
+        speak()
+    }
+}
+        
+
+        
